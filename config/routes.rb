313fc4a44947af05e561aa6b2bfb3378a49810ad
Brandy McKinney <missearlg80@gmail.com>
2021-07-11 18:45:50 -0400

    not sure

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/charities" => "charities#index"
   post "/charities" => "charities#create"
   get "/charities/:id" => "charities#show"
-  # patch "/charities/:id" => "charities#update"
+  patch "/charities/:id" => "charities#update"
 
   post "/users" => "users#create"
   get "/users" => "users#index"
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   post "/carted_products" => "carted_products#create"
-  patch "/carted_products" => "carted_products#update"
+  # patch "/carted_products/:id" => "carted_products#update"
   get "/carted_products" => "carted_products#index"
   get "/carted_products/:id" => "carted_products#show"
   get "/carted_products/notify" => "carted_products#notify"
 end
+
+#which patch do I use?
