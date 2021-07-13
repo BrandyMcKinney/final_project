4c0f057a9ebd04772b74b7a93c4103297da2969b
Brandy McKinney <missearlg80@gmail.com>
2021-07-12 23:42:33 -0400

    added a migration photo to charities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_021332) do
+ActiveRecord::Schema.define(version: 2021_07_13_033038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_021332) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_06_11_021332) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
