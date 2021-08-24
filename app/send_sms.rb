61dba114c35362fd91fc78f3db85a3c9c2d10b2b
Brandy McKinney <missearlg80@gmail.com>
2021-08-23 20:48:28 -0400

    Update bundle for linux

--- app/send_sms.rb
+++ app/send_sms.rb
@@ -6,7 +6,7 @@ auth_token = ""
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = # Your Twilio number
-  to = "+14439445966" # Your mobile phone number
+  to = "+19999999999" # Your mobile phone number
 
 client.messages.create(
   from: from,
