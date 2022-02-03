1697b2e332972b02fe1392f62f33a98207a4a8bb
Brandy McKinney <missearlg80@gmail.com>
2022-02-02 20:58:44 -0500

    updated rake gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :charities, only: [:index, :show, :create, :update]
+  # root :to => 'index#index'  Error code states ActionController::RoutingError (No route matches [GET] "/"):
+
 
   get "/charities" => "charities#index"
   post "/charities" => "charities#create"
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
   get "/carted_products" => "carted_products#index"
   get "/carted_products/:id" => "carted_products#show"
   get "/carted_products/notify" => "carted_products#notify"
+
 end
 
