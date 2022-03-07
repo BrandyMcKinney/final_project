f1296cca7e639213f2f858a9a55b88dcbbee65c4
Brandy McKinney <missearlg80@gmail.com>
2022-03-07 18:04:16 -0500

    Add resources to routes for charities

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => 'charities#index'  #Error code states ActionController::RoutingError (No route matches [GET] "/"):
   
-  # resources :charities, only: [:index, :show, :create, :update]
+  resources :charities, only: [:index, :show, :create, :update]
   resources :carted_products, only: [:index, :show, :create, :notify]
   
   # resources :users, only: [:index, :create]
 
-  get "/charities" => "charities#index"
-  post "/charities" => "charities#create"
-  get "/charities/:id" => "charities#show"
-  patch "/charities/:id" => "charities#update"
+  # get "/charities" => "charities#index"
+  # post "/charities" => "charities#create"
+  # get "/charities/:id" => "charities#show"
+  # patch "/charities/:id" => "charities#update"
 
   post "/users" => "users#create"
   get "/users" => "users#index"
